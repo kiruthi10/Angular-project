896031601262aa694497834384f8430c92a8c31f
kiruthi10 <ryakirthi10@gmail.com>
2022-01-27 16:53:27 +0530

    basic requirements of recipe and shopping list components has been added

--- demo/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ demo/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] =[
+    new Recipe('A Test Recipe','The test recipe description',
+    'https://i.pinimg.com/originals/74/b9/55/74b955513795417b28ec3e1a36c7ae0a.jpg'),
+    new Recipe('A Test Recipe','The test recipe description',
+    'https://i.pinimg.com/originals/74/b9/55/74b955513795417b28ec3e1a36c7ae0a.jpg')
+  ];
 
   constructor() { }
 
--- demo/src/app/shopping-list/shopping-list.component.ts
+++ demo/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredients } from '../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+   ingredients: Ingredients[] = [
+     new Ingredients('Apples', 15),
+     new Ingredients('Onion', 3),
+     new Ingredients('Tomato', 4)
+   ];
 
   constructor() { }
 
